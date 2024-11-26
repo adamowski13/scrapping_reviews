41b2dd1abed2795c3b770e4f0d4841ebdbe1e05b
JulienDira <julien.dirassouyan@outlook.fr>
2024-11-26 15:57:59 +0100

    ajout de logs, modification de process_data pour enregistrement dans de nouveaux csv

--- utils/extract_sentiment.py
+++ utils/extract_sentiment.py
@@ -3,56 +3,55 @@ from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from transformers import pipeline
+from transformers import logging as transformers_logging
 import logging
 
 # Configurer le logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+transformers_logging.set_verbosity_error()
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 # Fonction d'analyse de sentiment avec TextBlob
-def _analyze_sentiment_textblob(text):  # Fonction privée (usage interne)
+def _analyze_sentiment_textblob(text):
     try:
-        # Utilisation de TextBlob pour obtenir la polarité (positif, négatif, neutre)
         analysis = TextBlob(text)
-        polarity = analysis.sentiment.polarity
-        return polarity
+        return analysis.sentiment.polarity
     except Exception as e:
-        logging.error(f"Erreur dans l'analyse avec TextBlob: {e}")
+        logger.error(f"Erreur dans l'analyse avec TextBlob: {e}")
         return None
 
 # Fonction d'analyse de sentiment avec VADER
-def _analyze_sentiment_vader(text):  # Fonction privée (usage interne)
+def _analyze_sentiment_vader(text):
     analyzer = SentimentIntensityAnalyzer()
     try:
-        # Analyser le texte et renvoyer le score de sentiment
         sentiment_score = analyzer.polarity_scores(text)
-        score = sentiment_score['compound']  # Renvoie un score global de sentiment
-        return score  # Score global
+        return sentiment_score['compound']
     except Exception as e:
-        logging.error(f"Erreur dans l'analyse avec VADER: {e}")
+        logger.error(f"Erreur dans l'analyse avec VADER: {e}")
         return None
 
 # Appliquer TF-IDF sur le contenu pour extraire les mots-clés
 def extract_keywords(df, n_keywords=10):
+    logger.info("Début de l'extraction des mots-clés")
     try:
         vectorizer = TfidfVectorizer(stop_words='english', max_features=50)
         tfidf_matrix = vectorizer.fit_transform(df['Content'])
 
-        # Récupérer les mots-clés en fonction de leur score TF-IDF
         feature_names = vectorizer.get_feature_names_out()
         docterm = pd.DataFrame(tfidf_matrix.todense(), columns=feature_names)
 
-        sorted_indices = tfidf_matrix.sum(axis=0).argsort()[0, ::-1]  # Tri des indices des mots-clés
+        sorted_indices = tfidf_matrix.sum(axis=0).argsort()[0, ::-1]
         keywords = [feature_names[i] for i in sorted_indices[:n_keywords]]
-        
+
+        logger.info("Extraction des mots-clés terminée avec succès")
         return pd.DataFrame(docterm)  # keywords, docterm
     except Exception as e:
-        logging.error(f"Erreur dans l'extraction des mots-clés: {e}")
+        logger.error(f"Erreur dans l'extraction des mots-clés: {e}")
         return None
 
 # Fonction d'analyse de sentiment avec BERT
-def _analyze_sentiment_bert(text):  # Fonction privée (usage interne)
+def _analyze_sentiment_bert(text):
     try:
-        # Initialiser le pipeline de sentiment avec le modèle BERT
         analyzer = pipeline(
             task='text-classification',
             model="nlptown/bert-base-multilingual-uncased-sentiment",
@@ -69,34 +68,26 @@ def _analyze_sentiment_bert(text):  # Fonction privée (usage interne)
         
         result = analyzer(text, return_all_scores=True)
         max_result = max(result[0], key=lambda x: x['score'])
-        label = label2emotion[max_result['label']]  # Map the label to emotion
-        prob = round(max_result['score'] * 100, 2)  # Convert probability to percentage
+        label = label2emotion[max_result['label']]
+        prob = round(max_result['score'] * 100, 2)
         
         return label, prob
     except Exception as e:
-        logging.error(f"Erreur dans l'analyse avec BERT: {e}")
+        logger.error(f"Erreur dans l'analyse avec BERT: {e}")
         return None, None
 
 # Analyse des sentiments sur l'ensemble du dataframe
 def analyse_all_sentiments(data_frame):
+    logger.info("Début de l'analyse des sentiments sur le DataFrame")
     try:
-        total_lines = len(data_frame)
-        lines_processed = 0
-
-        # Utilisation des fonctions privées pour l'analyse des sentiments
+        # Ajouter les colonnes d'analyse des sentiments
         data_frame['Sentiment_TextBlob'] = data_frame['Content'].apply(lambda text: _analyze_sentiment_textblob(text))
         data_frame['Sentiment_VADER'] = data_frame['Content'].apply(lambda text: _analyze_sentiment_vader(text))
-
-        # Utilisation de la fonction BERT pour l'analyse de sentiment avec .apply()
-        data_frame[['sentiment_label', 'sentiment_prob']] = data_frame['Content'].apply(
-            lambda x: pd.Series(_analyze_sentiment_bert(x))
-        )
-
-        # Incrémenter le nombre de lignes traitées et afficher la progression
-        lines_processed = len(data_frame)
-        logging.info(f"Nombre de lignes traitées : {lines_processed}/{total_lines}")
-        
+        # data_frame[['sentiment_label', 'sentiment_prob']] = data_frame['Content'].apply(
+        #    lambda x: pd.Series(_analyze_sentiment_bert(x))
+        #)
+        logger.info("Analyse des sentiments terminée avec succès")
         return data_frame
     except Exception as e:
-        logging.error(f"Erreur dans l'analyse des sentiments pour le dataframe: {e}")
+        logger.error(f"Erreur dans l'analyse des sentiments : {e}")
         return None
